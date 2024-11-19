7ee6cee7639db3fb57ab2ba05c04cae80600e60f
Andrew Matysiak <andrewmatysiak@hotmail.com>
2024-11-20 06:55:53 +1100

    added brush styles and positioning.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 console.log('Hello World!');
 
-document.addEventListener('mousemove', (ev) => {
+document.addEventListener('mousemove', function(ev){
 
-  console.log(`X: ${ev.layerX}  Y:${ev.layerY}`);
   
+
+  //console.log(ev.pageX, ev.pageY);
+
+  const brushNode = document.createElement('div');
+
+  brushNode.className = 'location';
+
+  const brushSize = 16;
+  
+  brushNode.style.backgroundColor = 'red';
+  brushNode.style.width = brushSize + 'px';
+  brushNode.style.height = brushSize + 'px';
+  brushNode.style.borderRadius = 50 + '%';
+
+  brushNode.style.top = ev.pageY+ 'px'
+  brushNode.style.left = ev.pageX + 'px'
+
+
+  document.body.appendChild(brushNode);
 });
+
