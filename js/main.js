be56d0d22ef570f1f9b2d4c66b1825ec0771ae39
Andrew Matysiak <andrewmatysiak@hotmail.com>
2024-11-21 19:31:02 +1100

    added a cascade effect and removed scrolling effect.

--- js/main.js
+++ js/main.js
@@ -4,18 +4,19 @@ const randomNumber = function(){
 
   let number = Math.floor(Math.random() * 256);
   return number;
-}
+
+}; //randomNumber()
 
 const drawDot = function(locationY, locationX){
+
   const brushNode = document.createElement('div');
 
   brushNode.className = 'location';
 
-  const brushSize = 50;
+  const brushSize = 100;
   
   // brushNode.style.backgroundColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
   brushNode.style.backgroundColor = `hsla(${hueCounter}, 100%, 50%, 0.65)`;
-  hueCounter++;
   
   brushNode.style.width = brushSize + 'px';
   brushNode.style.height = brushSize + 'px';
@@ -25,8 +26,9 @@ const drawDot = function(locationY, locationX){
 
   document.body.appendChild(brushNode);
 
-}
+  hueCounter++;
 
+}; //drawDot()
 
 
 let hueCounter = 0; // will reset to 0 at 361 etc
@@ -34,9 +36,32 @@ let hueCounter = 0; // will reset to 0 at 361 etc
 document.addEventListener('mousemove', function(ev){
 
   if (ev.shiftKey){
-
     drawDot(ev.pageY, ev.pageX);
+    console.log(ev.pageY, ev.pageX);
+  }
+
+}); //'mousemove' addEventListener()
+
+
+//screensaver
+// setInterval(function(){
+//   drawDot(Math.random() * window.innerHeight, Math.random() * window.innerWidth);
+// }, 50)
+
+
+setInterval(function(){
+
+  const allBrushes = document.querySelectorAll('.location');
+
+  for(const brush of allBrushes){
+    
+     const topNum = parseInt(brush.style.top);
+     brush.style.top = (topNum + 10) + 'px';
 
   }
-});
 
+
+  if(topNum > window.innerHeight){
+    brush.remove();
+  }
+}, 15)
